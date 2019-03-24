1d08363d181d6a96f405038ca797ccb091098c2b
xingheng <15555306200@163.com>
2019-03-24 11:58:57 +0800

    add search.vue to city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
       <city-header></city-header>
+      <city-search></city-search>
   </div>
 </template>
 
 
 <script>
 import CityHeader from './components/Header.vue'
+import CitySearch from './components/Search.vue'
 export default {
   name:'City',
   components :{
       CityHeader,
+      CitySearch
   }
 }
 </script>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+     <div class="search">
+         <input class="search-input" type="text" placeholder="输入城市名或者拼音">
+     </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name:'CitySearch',
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
+    .search
+        height .72rem
+        background $bg-color
+        padding  0 .1rem
+        .search-input
+            box-sizing border-box
+            height .62rem
+            line-height  .62rem
+            width 100%
+            text-align center
+            border-radius .06rem
+            color #666
+            padding 0 .1rem
+
+
+</style>
